b6e7d98406bdbb7bf39305db745a1ffa827855ee
darshan3899 <dmshah.3899@gmail.com>
2020-04-21 20:46:04 +0530

    Updates

---
+++ precache-manifest.a113f847cde519b9697fcb4018355cca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bd668070a581b3d896dd1406d07ea4",
+    "url": "/React-Advice-App/index.html"
+  },
+  {
+    "revision": "f4fa50ed3ec1e7dbeeec",
+    "url": "/React-Advice-App/static/css/main.76607859.chunk.css"
+  },
+  {
+    "revision": "5a06d3d7dc9d8f02193e",
+    "url": "/React-Advice-App/static/js/2.f0fc2aad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Advice-App/static/js/2.f0fc2aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4fa50ed3ec1e7dbeeec",
+    "url": "/React-Advice-App/static/js/main.89ef0a2b.chunk.js"
+  },
+  {
+    "revision": "fa3f27a772f9ff4ff705",
+    "url": "/React-Advice-App/static/js/runtime-main.d5ed4843.js"
+  }
+]);
